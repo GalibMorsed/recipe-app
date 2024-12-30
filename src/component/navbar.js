8a718baa0d92716a025bc8c73c0827b8f09b3e0f
Galib Morsed <morsedgalib982@gmail.com>
2024-12-30 23:38:04 +0530

    initial commit

---
+++ src/component/navbar.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Sidebar from "./sidebar";
+import { faHome, faList, faCog } from "@fortawesome/free-solid-svg-icons";
+
+export default function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  const links = [
+    {
+      name: "Home",
+      url: "/",
+      icon: faHome,
+    },
+    {
+      name: "Recipes",
+      url: "/recipes",
+      icon: faList,
+    },
+    {
+      name: "Settings",
+      url: "/settings",
+      icon: faCog,
+    },
+  ];
+
+  function colseSidebar() {
+    setSidebar(false);
+  }
+  return (
+    <>
+      <div className="navbar  container">
+        <a href="#!" className="logo">
+          C<span>oo</span>kingsHub
+        </a>
+        <div className="nav-links">
+          {links.map((link) => (
+            <a href="#!" key={link.name}>
+              {link.name}
+            </a>
+          ))}
+          {/*<a href="#!">Home</a>
+          <a href="#!">Recipes</a>
+          <a href="#!">Settings</a>*/}
+        </div>
+        <div
+          onClick={() => setSidebar(true)}
+          className={sidebar ? "sidebar-btn active" : "sidebar-btn"}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+      {sidebar && <Sidebar close={colseSidebar} links={links} />}
+    </>
+  );
+}
